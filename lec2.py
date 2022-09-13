bc173e3c23593fc3e3ba075aba1ffc6fe88fc807
sober-baby <james19209@hotmail.com>
2022-09-13 15:49:32 -0400

    Create lec2.py

---
+++ lec2.py
@@ -0,0 +1,32 @@
+#A computer program is a sequence of instructions to the computer
+#Python is a scripting programming language
+#ESC 180 is about solving problems using programming in Pytho
+#Evaluated on how well you can solve problems using programming
+#Exam 1. give u a problem and solve with python 2. give you a code and find error?
+
+#a = 5
+#if a ==5:
+#    b = 2
+#print(b)
+
+# conditionals "if - statements"
+
+# compute |n|; the absoulte value of n and print it
+
+# n = -4
+# if n<=0:
+#     print(-n)
+#     print('Made a negative num to positve')
+# else:
+#     print(n)
+# print('done')e
+
+grade = 98
+if grade >= 99:
+    print("Whee")
+elif grade >=95:
+    print("ok")
+elif grade>= 92:
+    print("meh")
+else:
+    print("Alas")
